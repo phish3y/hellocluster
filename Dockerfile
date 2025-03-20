f0d6e3c46c05afa46378f47939ae40a32dfaafc3
evan <evan.logsdon@onxmaps.com>
2025-03-20 11:28:41 -0700

    infra setup

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM rust:1.83 AS builder
-WORKDIR /usr/src/wasm
+WORKDIR /usr/src/hellocluster
 
 RUN mkdir -p src && echo "fn main() {}" > src/main.rs
 COPY Cargo.lock Cargo.toml ./
@@ -11,5 +11,5 @@ RUN cargo build --release --target-dir /usr/local/cargo/bin
 
 FROM debian:stable-slim@sha256:dbab92bea4d20d665d158151d5c06fa8d205ab930b344ba949ef323fe98fa663
 RUN apt-get update && apt-get install -y lsb-release ca-certificates && apt-get clean all
-COPY --from=builder /usr/local/cargo/bin/release/wasm /usr/local/bin/wasm
-ENTRYPOINT ["/usr/local/bin/wasm"]
+COPY --from=builder /usr/local/cargo/bin/release/hellocluster /usr/local/bin/hellocluster
+ENTRYPOINT ["/usr/local/bin/hellocluster"]
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,58 @@
-use axum::{response::Html, routing::get, Router};
+use std::fs;
+
+use axum::{body::Body, http::Response, response::{Html, IntoResponse}, routing::get, Router};
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async {
-        Html("
-            <!DOCTYPE html>
-            <html>
-                <head>
-                    <title>phish3y_cluster</title>
-                </head>
-                <body>
-                    <h1>phish3y_cluster</h1>
-                </body>
-            </html>
-        "
-        )
-    }));
+    let app = Router::new()
+        .route("/", get(index))
+        .route("/favicon.ico", get(favicon))
+        .route("/healthz", get(healthz))
+        .route("/readyz", get(readyz));
 
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn index() -> Html<&'static str> {
+    Html("
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>phish3y's cluster</title>
+                <link rel='icon' href='/favicon.ico' type='image/x-icon'>
+            </head>
+            <body>
+                <h3>phish3y's cluster</h3>
+            </body>
+        </html>
+    ")
+}
+
+async fn favicon() -> impl IntoResponse {
+    match fs::read("favicon.ico") {
+        Ok(bytes) => Response::builder()
+            .header("Content-Type", "image/x-icon")
+            .body(Body::from(bytes))
+            .unwrap(),
+        Err(_) => Response::builder()
+            .status(404)
+            .body(Body::empty())
+            .unwrap(),
+    }
+}
+
+async fn healthz() -> impl IntoResponse {
+    Response::builder()
+        .status(200)
+        .body(Body::empty())
+        .unwrap()
+}
+
+async fn readyz() -> impl IntoResponse {
+    Response::builder()
+        .status(200)
+        .body(Body::empty())
+        .unwrap()
 }
