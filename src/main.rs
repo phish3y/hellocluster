e4d1fa1a353423d6554fb91ca072b071c49ae613
evan <logsdonevan1@gmail.com>
2025-03-20 17:57:26 -0700

    try using kube api

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::fs;
 
 use axum::{body::Body, http::Response, response::{Html, IntoResponse}, routing::get, Router};
+use k8s_openapi::api::networking::v1::Ingress;
+use kube::{api::ListParams, Api, Client};
 use tokio::net::TcpListener;
 
 #[tokio::main]
@@ -16,6 +18,28 @@ async fn main() {
 }
 
 async fn index() -> Html<&'static str> {
+    let kube_client = Client::try_default().await.unwrap();
+    let ingress_api: Api<Ingress> = Api::all(kube_client);
+
+    let ingresses = ingress_api.list(&ListParams::default()).await.unwrap();
+    for ingress in ingresses {
+        println!("found an ingress");
+        if let Some(spec) = ingress.spec {
+            if let Some(rules) = spec.rules {
+                for rule in rules {
+                    if let Some(host) = rule.host {
+                        println!("{}", host);
+                    }
+                    if let Some(http) = rule.http {
+                        for path in http.paths {
+                            println!("path: {}", path.path.unwrap_or_default());
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     Html("
         <!DOCTYPE html>
         <html>
